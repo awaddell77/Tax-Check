946141592225cabc300af3636d208318f8c80643
awaddell77 <waddell.andrew@gmail.com>
2019-12-14 11:05:06 -0800

    Updates

--- Tax_conn.py
+++ Tax_conn.py
@@ -6,7 +6,7 @@ class Tax_conn:
 		self.dbase= 'tax_rates.db'
 		self.table = 'tax_rates'
 		self._conn = sqlite3.connect(self.dbase)
-		#using dict instead of list because we need 1-1 (0) to map to december 
+		#using dict instead of list because we need 1-1 (0) and 12 to map to december 
 		self._months = {1:"january", 2:"february", 3:"march", 4:"april", 5:"may", 6:"june", 7:"july", 8:"august", 9:"september", 10:"october", 11:"november", 12:"december", 0:"december"}
 		self.cursor = self._conn.cursor()
 	def execute_query(self, comm):
